85e37f7362575936299adb6b2908979e74f3f106
sys72740hro <Hcbdag@635>
2022-02-08 14:21:14 +0100

    Eerste push examen training Gouda

--- edit.php
+++ edit.php
@@ -3,130 +3,133 @@
 
 
 <?php
-       
-       include('connection.php');
-       include('header.php');
-       
-        ?>  
+
+include('connection.php');
+include('header.php');
+
+?>
 <body>
 
-    <div id="wrapper">
+<div id="wrapper">
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">CRUD Using PHP/MySQL</a>
-            </div>
-     
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">CRUD Using PHP/MySQL</a>
+        </div>
 
-        <div id="page-wrapper">
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li class="active">
+                    <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                </li>
 
-            <div class="container-fluid">
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                           PHP CRUD <small>Create, Read, Update and Delete</small>
-                        </h1>
-                       
-                    </div>
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        PHP CRUD <small>Create, Read, Update and Delete</small>
+                    </h1>
+
                 </div>
-                <!-- /.row -->
-<?php 
-$query = 'SELECT * FROM people
+            </div>
+            <!-- /.row -->
+            <?php
+            $query = 'SELECT * FROM people
               WHERE
-              people_id ='.$_GET['id'];
-            $result = mysqli_query($db, $query) or die(mysqli_error($db));
-              while($row = mysqli_fetch_array($result))
-              {   
-                $zz= $row['people_id'];
-                $i= $row['first_name'];
-                $a=$row['last_name'];
-                $b=$row['mid_name'];
-                $c=$row['address'];
-                $d=$row['contact'];
-                $e=$row['comment'];
-             
-              }
-              
-              $id = $_GET['id'];
-         
-?>
+              people_id =' . $_GET['id'];
+            $result = $conn->prepare($query);
+            $result->execute();
+            while ($row = $result->fetch(pdo::FETCH_ASSOC))
+            {
+                $zz = $row['people_id'];
+                $i = $row['first_name'];
+                $a = $row['last_name'];
+                $b = $row['mid_name'];
+                $c = $row['address'];
+                $d = $row['contact'];
+                $e = $row['comment'];
 
-             <div class="col-lg-12">
-                  <h2>Edit Records</h2>
-                      <div class="col-lg-6">
+            }
 
-                        <form role="form" method="post" action="edit1.php">
-                            
-                            <div class="form-group">
-                                <input type="hidden" name="id" value="<?php echo $zz; ?>" />
-                            </div>
-                            <div class="form-group">
-                              <input class="form-control" placeholder="First Name" name="firstname" value="<?php echo $i; ?>">
-                            </div>
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Last Name" name="lastname" value="<?php echo $a; ?>">
-                            </div> 
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Middle Name" name="Middlename" value="<?php echo $b; ?>">
-                            </div> 
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Address" name="Address" value="<?php echo $c; ?>">
-                            </div> 
-                            <div class="form-group">
-                              <input class="form-control" placeholder="Contact" name="Contact" value="<?php echo $d; ?>">
-                            </div> 
-                            <div class="form-group">
-                             <label>Comment</label>
-                              <textarea class="form-control" rows="3"  name="comment"><?php echo $e; ?></textarea>
-                            </div>  
-                            <button type="submit" class="btn btn-default">Update Record</button>
-                         
+            $id = $_GET['id'];
+
+            ?>
+
+            <div class="col-lg-12">
+                <h2>Edit Records</h2>
+                <div class="col-lg-6">
+
+                    <form role="form" method="post" action="edit1.php">
+
+                        <div class="form-group">
+                            <input type="hidden" name="id" value="<?php echo $zz; ?>"/>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="First Name" name="firstname"
+                                   value="<?php echo $i; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Last Name" name="lastname"
+                                   value="<?php echo $a; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Middle Name" name="Middlename"
+                                   value="<?php echo $b; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Address" name="Address" value="<?php echo $c; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Contact" name="Contact" value="<?php echo $d; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label>Comment</label>
+                            <textarea class="form-control" rows="3" name="comment"><?php echo $e; ?></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Update Record</button>
 
 
-                      </form>  
-                    </div>
+                    </form>
                 </div>
-                
             </div>
-            <!-- /.container-fluid -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
 
     </div>
-    <!-- /#wrapper -->
+    <!-- /#page-wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+</div>
+<!-- /#wrapper -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="js/plugins/morris/raphael.min.js"></script>
+<script src="js/plugins/morris/morris.min.js"></script>
+<script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
